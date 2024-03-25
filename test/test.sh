ec3b3c3895118b11b1f236dd0b43280717c97451
Tom van Dijk <tom@tvandijk.nl>
2024-03-25 20:29:41 +0100

    Improvements to the test script

--- test/test.sh
+++ test/test.sh
@@ -13,17 +13,21 @@ fi
 
 for e in `ls -XrS ../examples/*hoa`; do
     f=$e
-    for s in sym tl; do
-        for o in "" "--bisim-game" "--bisim-game --bisim" "--bisim" "--isop" "--onehot" "--onehot --isop"; do
-            echo "solving $f using solver $s and options $o"
+    #for s in sym tl; do
+        #for o in "" "--onehot" "--bisim" "--isop" "--bisim --isop"; do
+    for s in sym; do
+        #for o in "--isop --onehot"; do
+        for o in "--sop"; do
+            echo -e "\033[32m$f, solver $s, options $o\033[m"
             ../build/knor $f --$s $o -v -a > $f-output-$s 2> $f-log
             res=$?
             if [[ $res == 10 ]]; then
-                echo "preparing model checking..."
+                # echo "preparing model checking..."
                 cat $f | ../test/hoa2aig > $f-monitor.aag
-                cat $f-output-$s > $f-sol.aag
-                ../test/combine-aiger $f-monitor.aag $f-sol.aag > $f-combined.aag
-                echo "running model checker..."
+                # tail -n +2 $f-output-$s > $f-sol.aag
+                ../test/combine-aiger $f-monitor.aag $f-output-$s > $f-combined.aag
+                # echo "running model checker..."
+                # cat "read_aiger_model -i $f-combined.aag; encode_variables; build_boolean_model; check_ltlspec_ic3; quit" 
                 echo "read_aiger_model -i $f-combined.aag; encode_variables; build_boolean_model; check_ltlspec_ic3; quit" | timeout -k 10 10 $nuXmv -int > $f-res 2>&1
                 # sed -n '/^nuXmv >/,$p' $f-res
                 cat $f-res | grep 'specification .* is '
