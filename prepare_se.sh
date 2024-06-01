0d7fe5b518deba65d761094a7eab8d0b17b72608
Tom van Dijk <tom@tvandijk.nl>
2024-06-01 22:48:54 +0200

    Prepare for SYNTCOMP 2024

--- prepare_se.sh
+++ prepare_se.sh
@@ -3,6 +3,6 @@ DIR=`dirname $0`/se
 cd ${DIR}
 rm -rf source ../knor.tar.gz
 mkdir source
-cp -r ../sylvan ../abc ../oink ../src ../CMakeLists.txt source
+cp -r ../libs ../src ../CMakeLists.txt source
 tar zcfv ../knor.tar.gz *
 rm -rf source
